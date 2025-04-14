190ec4e55bd7d0b43d5bf10a1502d18d6d075862
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2025-04-14 16:41:54 +0700

    fix snake segment to tail node snake and feat struct snake, function add tail, function free tail

--- include/snake.h
+++ include/snake.h
@@ -1,6 +1,7 @@
 #ifndef SNAKE_H
 #define SNAKE_H
 
+// nentuin arah pergerakan
 enum eDirection
 {
     STOP = 0,
@@ -10,8 +11,27 @@ enum eDirection
     DOWN
 };
 
-struct SnakeSegment
+// struct untuk menyimpan posisi tail
+struct TailNode
 {
-    int x;
-    int y;
+    int x, y;
+    TailNode *next;
 };
+
+// struct buat snake menggunakan linked list
+struct Snake
+{
+    int x, y;
+    int fruitX, fruitY;
+    int score;
+    TailNode *tail;
+    int tailLength;
+    eDirection dir;
+    bool gameOver;
+};
+
+// prototype fungsi-fungsi pengelolaan tail (linked list)
+void AddTailSegment(Snake *s, int newX, int newY);
+void FreeTail(Snake *s);
+
+#endif
